915c549e872017f0c12b61f708ecea0d65ecb4b8
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-07-01 18:20:51 -0300

    First commit

---
+++ src/main/java/br/com/cardgame/jeff/JeffApplication.java
@@ -0,0 +1,13 @@
+package br.com.cardgame.jeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cardgame/jeff/JeffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardgame.jeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
